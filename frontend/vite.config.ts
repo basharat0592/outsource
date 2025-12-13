29ed59d5e63c22f56e716670cef250783cc64b83
basharat0592 <97874569+basharat0592@users.noreply.github.com>
2025-12-13 19:40:41 +0500

    configs for publish

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 1600,       // <-- set dev server port here
+    strictPort: true, // optional: fail if port 1600 is already in use
+  },
 })
